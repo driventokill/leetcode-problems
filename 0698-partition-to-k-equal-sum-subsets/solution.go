6e166a6710eb4d7396e829a35d567ede40385278
Yang Fang <fangyang.frank@bytedance.com>
2022-09-04 22:48:30 +0800

    + 698 partition to k equal sum subsets

---
+++ 0698-partition-to-k-equal-sum-subsets/solution.go
@@ -0,0 +1,97 @@
+// https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
+package partitiontokequalsumsubsets
+
+func canPartitionKSubsets(nums []int, k int) bool {
+	if k > len(nums) {
+		return false
+	}
+
+	var sum int
+	for _, v := range nums {
+		sum += v
+	}
+	if sum%k != 0 {
+		return false
+	}
+
+	target := sum / k
+
+	return byBucket(nums, k, target)
+}
+
+// ----------- traverse by bucket -----------------------------------
+
+func byBucket(nums []int, k, target int) bool {
+	used := make([]bool, len(nums))
+	return backtrackByBucket(k, 0, nums, 0, used, target)
+}
+
+// k: current bucket to be filled
+func backtrackByBucket(k, bucket int, nums []int, start int, used []bool, target int) bool {
+	if k == 0 {
+		// every bucket is fullfilled
+		return true
+	}
+
+	if bucket == target {
+		// current bucket is full
+		// select nums for the next bucket
+		return backtrackByBucket(k-1, 0, nums, 0, used, target)
+	}
+
+	// find usable num from start
+	for i := start; i < len(nums); i++ {
+		if used[i] {
+			continue
+		}
+
+		if nums[i]+bucket > target {
+			continue
+		}
+
+		// make a decision
+		used[i] = true
+		bucket += nums[i]
+		// decide if the next number should be filled
+		if backtrackByBucket(k, bucket, nums, i+1, used, target) {
+			return true
+		}
+
+		// revert
+		used[i] = false
+		bucket -= nums[i]
+	}
+
+	return false
+}
+
+// ----------- traverse by num --------------------------------------
+
+func byNum(nums []int, k, target int) bool {
+	buckets := make([]int, k)
+	return backtrackByNum(buckets, nums, 0, target)
+}
+
+func backtrackByNum(sums []int, nums []int, index, target int) bool {
+	if index == len(nums) {
+		for i := range sums {
+			if sums[i] != target {
+				return false
+			}
+		}
+		return true
+	}
+
+	for i := range sums {
+		if sums[i]+nums[index] > target {
+			continue
+		}
+		sums[i] += nums[index]
+		if backtrackByNum(sums, nums, index+1, target) {
+			return true
+		}
+		sums[i] -= nums[index]
+	}
+
+	return false
+}
